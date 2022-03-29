522b616a55071a7d731e751094122e921ebfd033
Stefan Rotaru <stefanrotaru@hotmail.com>
2022-03-29 12:28:56 +0300

    fixed indentation

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import './ImageLinkForm.css'
+import React from "react";
+import "./ImageLinkForm.css";
 
 const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
-	return (
-		<div>
-			<p className='f3'>
-				{"I'm a smart brain programmed to recognise faces in any given picture."}
-			</p>
-			<p className='f3'>
-				{"Try me out!"}
-			</p>
-			<div className='center'>
-				<div className='form center pa4 br3 shadow-5'>
-					<input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
-					<button 
-						className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
-						onClick={onButtonSubmit}
-					>Detect</button>
-				</div>
-			</div>
-		</div>
-	);
-}
+  return (
+    <div>
+      <p className="f3">
+        {
+          "I'm a smart brain programmed to recognise faces in any given picture."
+        }
+      </p>
+      <p className="f3">{"Try me out!"}</p>
+      <div className="center">
+        <div className="form center pa4 br3 shadow-5">
+          <input
+            className="f4 pa2 w-70 center"
+            type="text"
+            onChange={onInputChange}
+          />
+          <button
+            className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
+            onClick={onButtonSubmit}
+          >
+            Detect
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ImageLinkForm;
